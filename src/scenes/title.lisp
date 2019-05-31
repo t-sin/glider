40dff921fc026e356c158f62a689c90b3ac3f50c
t-sin <shinichi.tanaka45@gmail.com>
2019-06-01 00:40:42 +0900

    Regular polygons

--- src/scenes/title.lisp
+++ src/scenes/title.lisp
@@ -10,26 +10,48 @@
   (:export #:init-title))
 (in-package #:glider/scenes/title)
 
+(defun make-rpolygon-drawer (r n)
+  (let ((pts (coerce (loop
+                       :for i :from 1 :upto n
+                       :for deg := (* i (/ 360 n))
+                       :collect (cons (* r (cos (to-rad deg)))
+                                      (* r (sin (to-rad deg)))))
+                     'simple-vector)))
+    (lambda (renderer a)
+      (set-render-draw-color renderer 100 200 200 255)
+      (loop
+        :for i :from 0 :below n
+        :for p1 := (svref pts i)
+        :for p2 := (svref pts (mod (1+ i) n))
+        :do (render-draw-line renderer
+                              (floor (+ (actor-x a) (car p1)))
+                              (floor (+ (actor-y a) (cdr p1)))
+                              (floor (+ (actor-x a) (car p2)))
+                              (floor (+ (actor-y a) (cdr p2))))))))
+
 (defun make-event ()
-  `((0 . (:fire 0 0 $id))))
-
-(defun make-drawer ()
-  (lambda (renderer a)
-    ))
-
+  `((0 . (:fire 100 400 ,#'$id ,(make-rpolygon-drawer 20 3)))
+    (0 . (:fire 210 400 ,#'$id ,(make-rpolygon-drawer 20 4)))
+    (0 . (:fire 320 400 ,#'$id ,(make-rpolygon-drawer 20 5)))
+    (0 . (:fire 100 510 ,#'$id ,(make-rpolygon-drawer 20 6)))
+    (0 . (:fire 210 510 ,#'$id ,(make-rpolygon-drawer 20 7)))
+    (0 . (:fire 320 510 ,#'$id ,(make-rpolygon-drawer 20 8)))))
 
 (defun init-title (g)
   (let ((actors (init-actors)))
     (loop
       :for a :across actors
-      :do (setf (actor-draw-fn a) (make-drawer)
-                (actor-sfn a) (lambda () nil)))
+      :do (setf (actor-sfn a) (lambda () nil)))
     (setf (global-vm g) (make-vm :tick 0
                                  :actors actors
                                  :etable (make-event))))
   (lambda (renderer)
     (let ((vm (global-vm g)))
       (execute vm)
+      (render-clear renderer)
+      (set-render-draw-color renderer 0 0 25 255)
+      (set-render-draw-blend-mode renderer :blend)
+      (render-fill-rect renderer (make-rect 0 0 *screen-width* *screen-height*))
       (loop
         :for a :across (vm-actors vm)
         :when (actor-available? a)
